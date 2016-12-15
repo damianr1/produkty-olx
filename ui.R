18a9edc815215eaf2a1e2574ea1c88c3b3c47e9a
damianr1 <damian.rolek@gmail.com>
2016-12-15 21:36:40 +0100

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,25 @@
+# package (which generally comes preloaded).
+library(datasets)
+
+# Use a fluid Bootstrap layout
+fluidPage(    
+  
+  # Give the page a title
+  titlePanel("Najlepsze ogloszenia OLX"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      selectInput("max", "Liczba najlepszych:", 
+                  choices=c("3","5","10", "15", "20", "25", "30")),
+      hr()
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("OLXPlot")  
+    )   
+  )
+)
